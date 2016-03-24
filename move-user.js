32984f2b25f6d140abab97d3d9d0b8d879554153
qing@ciandt.com <qing@ciandt.com>
2016-03-24 16:29:06 +0800

    move_user ugrade

--- move-user.js
+++ move-user.js
@@ -1,26 +1,23 @@
 (function ($) {
 
   Drupal.behaviors.moveUserAdmin = {
-    attach: function (context, settings) {
 
+    attach: function (context, settings) {
       var formItemReassignUser = $('.form-item-reassign-user', context);
       var cancelMethodsWrapper = $("#edit-user-cancel-method", context);
-
       if ($('[value=move_user_reassign]').is(':checked')){
          formItemReassignUser.appendTo(cancelMethodsWrapper).show();
       } else {
         formItemReassignUser.hide();
       }
-
-      $(cancelMethodsWrapper).find('input').once('move-user-admin', function () {
-        $(this).change(function (e) {
+      $("#edit-user-cancel-method input").off().on("change", function (e) {
           if ($(e.target).val() == 'move_user_reassign') {
             formItemReassignUser.appendTo(cancelMethodsWrapper).fadeIn(200);
           } else {
             formItemReassignUser.hide();
           }
         });
-      }, context);
+
 
     }
   };
