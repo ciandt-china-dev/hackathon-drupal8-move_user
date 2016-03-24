cac139c671f79e31aa8441ebacd96274f92d387b
Xiaodan Wang <bala@ciandt.com>
2016-03-24 20:52:00 +0800

    Add webtest for move_user module

--- src/Tests/MoveUserTest.php
+++ src/Tests/MoveUserTest.php
@@ -8,7 +8,7 @@
 
 namespace Drupal\move_user\Tests;
 
-use Drupal\Simpletest\WebTestBase;
+use \Drupal\simpletest\WebTestBase;
 
 /**
  * Create two users and use one user to create few nodes, 
@@ -17,11 +17,51 @@ use Drupal\Simpletest\WebTestBase;
  *
  * @group move_user
  */
-class MoveUserTest extends WebTestBase {
+class MoveUserTest extends \Drupal\node\Tests\NodeTestBase {
+
+  protected $originalAuthor;
+
+  protected $newAuthor;
+
+  protected $createdNode;
+
+  public static $modules = array('move_user', 'user');
 
   /**
-   * Tests the assign the new user when move user.
+   * {@inheritdoc}
    */
-  public function testMoveUser() {
+  protected function setUp() {
+    parent::setUp();
+
+    $this->originalAuthor = $this->drupalCreateUser(array('create page content', 'edit own page content'));
+    // Create another admin user for delete the web_user
+    $this->newAuthor = $this->drupalCreateUser(array('create page content', 'edit own page content'));
+
+    // create a page node with current user
+    $this->createdNode = $this->drupalCreateNode(array('type' => 'page', 'uid' => $this->originalAuthor->id()));
+
+  }
+
+  /**
+   * Tests to reassign node to another user.
+   */
+  public function testCancelUserMoveUser() {
+    // Create administrator user and login
+    $admin_user = $this->drupalCreateUser(array('administer nodes', 'administer users'));
+    $this->drupalLogin($admin_user);
+
+    // Cancel the author page
+    $cancel_user_url = 'user/' . $this->originalAuthor->id() . '/cancel';
+    $this->drupalGet($cancel_user_url);
+
+    $edit = array();
+    $edit['user_cancel_method'] = 'move_user_reassign';
+    $edit['reassign_user'] = $this->newAuthor->getUserName() . ' (' . $this->newAuthor->id() . ')';
+    $edit['confirm'] = 1; 
+    $this->drupalPostForm($cancel_user_url, $edit, t('Cancel account'));
+
+    // check if the node's author is changed to newAuthor
+    $node = node_load($this->createdNode->id());
+    $this->assertEqual($node->getOwnerId(), $this->newAuthor->id(), 'Ensure that the node is reassigned to the new user correctly.');
   }
 }
