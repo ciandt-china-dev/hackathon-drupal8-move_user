a4c9340231d058641163a9ea0091f638f79c1a68
Xiaodan Wang <bala@ciandt.com>
2016-03-25 13:47:06 +0800

    Fix code standarings

--- move-user.js
+++ move-user.js
@@ -1,3 +1,7 @@
+/**
+ * @file
+ */
+
 (function ($) {
 
   Drupal.behaviors.moveUserAdmin = {
@@ -5,19 +9,20 @@
     attach: function (context, settings) {
       var formItemReassignUser = $('.form-item-reassign-user', context);
       var cancelMethodsWrapper = $("#edit-user-cancel-method", context);
-      if ($('[value=move_user_reassign]').is(':checked')){
+      if ($('[value=move_user_reassign]').is(':checked')) {
          formItemReassignUser.appendTo(cancelMethodsWrapper).show();
-      } else {
+      }
+      else {
         formItemReassignUser.hide();
       }
       $("#edit-user-cancel-method input").off().on("change", function (e) {
-          if ($(e.target).val() == 'move_user_reassign') {
-            formItemReassignUser.appendTo(cancelMethodsWrapper).fadeIn(200);
-          } else {
-            formItemReassignUser.hide();
-          }
-        });
-
+        if ($(e.target).val() == 'move_user_reassign') {
+          formItemReassignUser.appendTo(cancelMethodsWrapper).fadeIn(200);
+        }
+        else {
+          formItemReassignUser.hide();
+        }
+      });
 
     }
   };
--- src/Tests/MoveUserTest.php
+++ src/Tests/MoveUserTest.php
@@ -3,15 +3,15 @@
  * @file
  * Test case for testing the move_user module.
  *
- * This file contains the test cases to check if move user is performing correctly.
+ * This file contains the test cases to check if move user is done correctly.
  */
 
 namespace Drupal\move_user\Tests;
 
-use \Drupal\simpletest\WebTestBase;
-
 /**
- * Create two users and use one user to create few nodes, 
+ * Move user test case.
+ *
+ * Create two users and use one user to create few nodes,
  * then use the administrator user to delete the nodes created,
  * reassign the nodes to the second user.
  *
@@ -34,10 +34,10 @@ class MoveUserTest extends \Drupal\node\Tests\NodeTestBase {
     parent::setUp();
 
     $this->originalAuthor = $this->drupalCreateUser(array('create page content', 'edit own page content'));
-    // Create another admin user for delete the web_user
+    // Create another admin user for delete the web_user.
     $this->newAuthor = $this->drupalCreateUser(array('create page content', 'edit own page content'));
 
-    // create a page node with current user
+    // Create a page node with current user.
     $this->createdNode = $this->drupalCreateNode(array('type' => 'page', 'uid' => $this->originalAuthor->id()));
 
   }
@@ -46,22 +46,23 @@ class MoveUserTest extends \Drupal\node\Tests\NodeTestBase {
    * Tests to reassign node to another user.
    */
   public function testCancelUserMoveUser() {
-    // Create administrator user and login
+    // Create administrator user and login.
     $admin_user = $this->drupalCreateUser(array('administer nodes', 'administer users'));
     $this->drupalLogin($admin_user);
 
-    // Cancel the author page
+    // Cancel the author page.
     $cancel_user_url = 'user/' . $this->originalAuthor->id() . '/cancel';
     $this->drupalGet($cancel_user_url);
 
     $edit = array();
     $edit['user_cancel_method'] = 'move_user_reassign';
     $edit['reassign_user'] = $this->newAuthor->getUserName() . ' (' . $this->newAuthor->id() . ')';
-    $edit['confirm'] = 1; 
+    $edit['confirm'] = 1;
     $this->drupalPostForm($cancel_user_url, $edit, t('Cancel account'));
 
-    // check if the node's author is changed to newAuthor
+    // Check if the node's author is changed to newAuthor.
     $node = node_load($this->createdNode->id());
     $this->assertEqual($node->getOwnerId(), $this->newAuthor->id(), 'Ensure that the node is reassigned to the new user correctly.');
   }
+
 }
